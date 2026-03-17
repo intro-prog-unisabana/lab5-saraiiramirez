2571ee40dc1a8b5bed8f2e7b54eeabfda57d48bc
Sarai Ramirez <sarairace@unisabana.edu.co>
2026-03-17 12:24:34 -0500

    lab5

--- multiple_files.py
+++ multiple_files.py
@@ -3,8 +3,8 @@ from utils import flip, count_letters
 mensaje = input("Please type your message\n")
 
 invertido = flip(mensaje)
-cantidad = count_letters(mensaje, 'a')
+cantidad = count_letters(mensaje)
 
 mensaje_codificado = invertido + str(cantidad)
 
-print("Your encoded message is :", mensaje_codificado)
+print(f"Your encoded message is : {mensaje_codificado}")
--- utils.py
+++ utils.py
@@ -46,3 +46,4 @@ if __name__ == "__main__":
   print("This file is being run directly.")
 # FREEZE CODE END
 
+
