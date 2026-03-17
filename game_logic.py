b8cba8bc77d22dff08c9e4499a0abadb82ca7488
Sarai Ramirez <sarairace@unisabana.edu.co>
2026-03-16 22:13:31 -0500

    lab 5

--- game_logic.py
+++ game_logic.py
@@ -1,1 +1,20 @@
 from secret_number import seed_secret_numbers , generate_secret_number
+from response import input_response
+
+seed = int(input("Enter a seed number: "))
+seed_secret_numbers(seed)
+
+generate_value = generate_secret_number()
+
+tries = 0
+correcto = False
+
+while not correcto:
+    user_input = int(input("What is your guess: "))
+    tries +=1
+
+
+    mensaje, correcto = input_response(generate_value, user_input)
+    print(mensaje)
+
+print("It took you", tries, "tries!")
